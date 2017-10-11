2c05e70880dee8ae2570adbcf721bf5a677bcd6a
이진영 <mufasa@ijin-yeong-ui-MacBook-Pro.local>
2017-10-12 00:19:18 +0900

    socketio example

--- socketioEx01/socket.io.server.js
+++ socketioEx01/socket.io.server.js
@@ -19,8 +19,31 @@ server.listen(3000, function(){
 });
 
 // 소켓 서버 실행 
+/**
+ * 소켓 통신 종류 
+ * public            자신을 포함한 모든 클라이언트에 데이터 전달 
+ * broadcast         자신을 제외한 모든 클라이언트에 데이터 전달
+ * private           특정 클라이언트에 데이터 전달 
+ */
+var id = 0;
 var io = socketio.listen(server);
 io.sockets.on('connection', function(socket){
+    // id를 설정
+    id = socket.id;
 
+    // rint 이벤트 
+    socket.on('make', function(data){
+        // 클라이언트가 전송한 데이터를 출력
+        console.log('Client Send Data : ', data);
+
+        // 클라이언트에 smart 이벤트를 발생
+        // socket.emit('smart',data);
+        // public 통신
+        // io.sockets.emit('smart', data);              
+        // broadcast 
+        // socket.broadcast.emit('smart', data);
+        // 
+        io.sockets.to(id).emit('smart', data);
+    });
 });
 
